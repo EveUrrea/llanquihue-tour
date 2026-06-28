9d3b456d4485a0d5843ef9a6b1cee8d9dde11302
Evelyn Urrea <eve.urrea@duocuc.cl>
2026-06-28 19:42:59 -0400

    Implementación de PaseoLacustre

--- src/model/PaseoLacustre.java
+++ src/model/PaseoLacustre.java
@@ -1,4 +1,27 @@
 package model;
 
-public class PaseoLacustre {
+public class PaseoLacustre extends ServicioTuristico {
+
+    private String tipoEmbarcacion;
+
+    public PaseoLacustre(String nombre, int duracionHoras, String tipoEmbarcacion) {
+        super(nombre, duracionHoras);
+
+        if (tipoEmbarcacion == null || tipoEmbarcacion.trim().isEmpty()) {
+            throw new IllegalArgumentException("El tipo de embarcación no puede estar vacío.");
+        }
+
+        this.tipoEmbarcacion = tipoEmbarcacion;
+    }
+
+    public String getTipoEmbarcacion() {
+        return tipoEmbarcacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Paseo Lacustre | " +
+                super.toString() +
+                ", Tipo de embarcación: " + tipoEmbarcacion;
+    }
 }
