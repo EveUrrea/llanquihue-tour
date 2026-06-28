19f9d13c7c6d5d18be39db7ca1125dea12dce82b
Evelyn Urrea <eve.urrea@duocuc.cl>
2026-06-28 19:39:46 -0400

    Implementación de clase base ServicioTuristico

--- src/model/ServicioTuristico.java
+++ src/model/ServicioTuristico.java
@@ -1,4 +1,35 @@
 package model;
 
 public class ServicioTuristico {
+
+    private String nombre;
+    private int duracionHoras;
+
+    public ServicioTuristico(String nombre, int duracionHoras) {
+
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacío.");
+        }
+
+        if (duracionHoras <= 0) {
+            throw new IllegalArgumentException("La duración debe ser mayor que cero.");
+        }
+
+        this.nombre = nombre;
+        this.duracionHoras = duracionHoras;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getDuracionHoras() {
+        return duracionHoras;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre +
+                ", Duración: " + duracionHoras + " horas";
+    }
 }
