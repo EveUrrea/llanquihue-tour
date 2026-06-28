8b9fe324120e9f196a4c7e1bf491fba5d895f07b
Evelyn Urrea <eve.urrea@duocuc.cl>
2026-06-28 19:41:16 -0400

    Implementación de RutaGastronomica

--- src/model/RutaGastronomica.java
+++ src/model/RutaGastronomica.java
@@ -1,4 +1,27 @@
 package model;
 
-public class RutaGastronomica {
+public class RutaGastronomica extends ServicioTuristico {
+
+    private int numeroDeParadas;
+
+    public RutaGastronomica(String nombre, int duracionHoras, int numeroDeParadas) {
+        super(nombre, duracionHoras);
+
+        if (numeroDeParadas <= 0) {
+            throw new IllegalArgumentException("El número de paradas debe ser mayor que cero.");
+        }
+
+        this.numeroDeParadas = numeroDeParadas;
+    }
+
+    public int getNumeroDeParadas() {
+        return numeroDeParadas;
+    }
+
+    @Override
+    public String toString() {
+        return "Ruta Gastronómica | " +
+                super.toString() +
+                ", Número de paradas: " + numeroDeParadas;
+    }
 }
