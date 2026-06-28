44d0c61f8188d20129ffa4993605a6c6085a3620
Evelyn Urrea <eve.urrea@duocuc.cl>
2026-06-28 19:44:43 -0400

    Implementación de ExcursionCultural

--- src/model/ExcursionCultural.java
+++ src/model/ExcursionCultural.java
@@ -1,4 +1,27 @@
 package model;
 
-public class ExcursionCultural {
+public class ExcursionCultural extends ServicioTuristico {
+
+    private String lugarHistorico;
+
+    public ExcursionCultural(String nombre, int duracionHoras, String lugarHistorico) {
+        super(nombre, duracionHoras);
+
+        if (lugarHistorico == null || lugarHistorico.trim().isEmpty()) {
+            throw new IllegalArgumentException("El lugar histórico no puede estar vacío.");
+        }
+
+        this.lugarHistorico = lugarHistorico;
+    }
+
+    public String getLugarHistorico() {
+        return lugarHistorico;
+    }
+
+    @Override
+    public String toString() {
+        return "Excursión Cultural | " +
+                super.toString() +
+                ", Lugar histórico: " + lugarHistorico;
+    }
 }
