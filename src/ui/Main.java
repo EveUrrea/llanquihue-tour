13ce035d3cffc295980da25c52b4d2e8eeb57ace
Evelyn Urrea <eve.urrea@duocuc.cl>
2026-06-28 20:02:50 -0400

    Actualización de Main para mostrar servicios turísticos

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,6 +1,8 @@
 package ui;
 
 import data.GestorDatos;
+import data.GestorServicios;
+import model.ServicioTuristico;
 import model.Tour;
 import service.TourService;
 
@@ -11,7 +13,6 @@ public class Main {
     public static void main(String[] args) {
 
         GestorDatos gestor = new GestorDatos();
-
         ArrayList<Tour> tours = gestor.cargarTours();
 
         TourService servicio = new TourService();
@@ -20,5 +21,13 @@ public class Main {
         servicio.mostrarToursCaros(tours);
         servicio.buscarPorTipo(tours, "Cultural");
 
+        System.out.println("\n===== SERVICIOS TURÍSTICOS SEMANA 6 =====");
+
+        GestorServicios gestorServicios = new GestorServicios();
+        ServicioTuristico[] serviciosTuristicos = gestorServicios.obtenerServicios();
+
+        for (ServicioTuristico servicioTuristico : serviciosTuristicos) {
+            System.out.println(servicioTuristico);
+        }
     }
 }
